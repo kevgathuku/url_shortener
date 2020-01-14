d576dba1b129915c82be5c7935ecb3d7de812b1e
Kevin Ndung'u <kevgathuku@gmail.com>
2020-01-14 13:21:16 +0300

    Init url shortener app

---
+++ lib/url_shortener.ex
@@ -0,0 +1,18 @@
+defmodule UrlShortener do
+  @moduledoc """
+  Documentation for UrlShortener.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UrlShortener.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
